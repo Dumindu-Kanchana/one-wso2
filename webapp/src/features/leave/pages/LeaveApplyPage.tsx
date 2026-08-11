b10cf82df871ab556791644cf7deb0538907de9d
Dumindu-Kanchana <duminduk@wso2.com>
2026-08-11 11:45:47 +0530

    Add no search result message and fetch-loading feedback to employee pickers

--- webapp/src/features/leave/pages/LeaveApplyPage.tsx
+++ webapp/src/features/leave/pages/LeaveApplyPage.tsx
@@ -304,6 +304,8 @@ function ApplyForm() {
           value={recipients}
           onChange={(_e, v) => setRecipients(v as string[])}
           loading={employees.isLoading}
+          loadingText="Loading employees…"
+          noOptionsText="No employees found"
           disableListWrap
           ListboxComponent={VirtualizedListbox}
           renderInput={(params) => (
